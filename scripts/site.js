7a9291fff88362be3c93f22e64534969e6e566c0
Nicolas Lindberg <nicolas@nectima.se>
2019-05-02 00:32:49 +0200

    Add network site links

--- scripts/site.js
+++ scripts/site.js
@@ -27,6 +27,7 @@
   document.getElementById('mobile-nav-toggle').addEventListener('click', function(el) {
     el.target.classList.toggle('is-visible');
     document.getElementById('site-nav').classList.toggle('is-visible');
+    document.body.classList.toggle('no-scroll');
   });
 
 

5e4f5b94347fe5f6ecc8b2b185bbe6caf817dc6a
Nicolas Lindberg <nicolas@nectima.se>
2019-05-02 21:36:28 +0200

    Fix image loader

---
+++ scripts/controllers/ResponsiveImageLoader.js
@@ -0,0 +1,43 @@
+import { ImageLoader, Tweaks } from '@squarespace/core';
+import debounce from 'lodash/debounce';
+
+/**
+ * Initialize Squarespace image loader
+ */
+function ResponsiveImageLoader (element) {
+
+  // Load all images via Squarespace's Responsive ImageLoader
+  function loadAllImages() {
+
+    const images = element.querySelectorAll('img[data-src]');
+
+    for (var i = 0; i < images.length; i++) {
+      ImageLoader.load(images[i], {
+        load: true
+      });
+    }
+    
+  }
+
+  // The event subscription that loads images when the page is ready
+  document.addEventListener('DOMContentLoaded', loadAllImages);
+
+  // Tweak handler
+  const tweaksFromDOM = element.getAttribute('data-tweaks');
+
+  if (tweaksFromDOM && tweaksFromDOM.length > 0) {
+
+    const tweaks = tweaksFromDOM.split(',').map(function (tweakName) {
+      return tweakName.trim();
+    });
+
+    Tweak.watch(tweaks, loadAllImages);
+
+  }
+
+  // The event subscription that reloads images on resize
+  const debouncedResize = debounce(loadAllImages, 40);
+  window.addEventListener('resize', debouncedResize);
+}
+
+export default ResponsiveImageLoader;
--- scripts/site.js
+++ scripts/site.js
@@ -5,9 +5,9 @@ import controller from '@squarespace/controller';
 
 import BlogLayout from './controllers/BlogLayout';
 import HeaderNav from './controllers/HeaderNav';
-import ImageLoader from './controllers/ImageLoader';
+import ResponsiveImageLoader from './controllers/ResponsiveImageLoader';
 
 // Bind controllers
 controller.register('BlogLayout', BlogLayout);
 controller.register('HeaderNav', HeaderNav);
-controller.register('ImageLoader', ImageLoader);
+controller.register('ResponsiveImageLoader', ResponsiveImageLoader);

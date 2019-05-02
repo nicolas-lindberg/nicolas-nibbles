54cca771743df6669dee80a5660a631de9846fe0
Nicolas Lindberg <nicolas@nectima.se>
2019-05-02 18:26:17 +0200

    Header fixes

--- scripts/controllers/HeaderNav.js
+++ scripts/controllers/HeaderNav.js
@@ -4,11 +4,29 @@
  */
 function HeaderNav(element) {
 
-  element.querySelector('#mobile-nav-toggle').addEventListener('click', function(el) {
-    el.target.classList.toggle('is-visible');
+  const mobileNavToggle = element.querySelector('#mobile-nav-toggle');
+
+  mobileNavToggle.addEventListener('click', function(el) {
+    el = el.target;
+
+    // Toggle visibility state
+    el.classList.toggle('is-visible');
     element.querySelector('#site-nav').classList.toggle('is-visible');
     document.body.classList.toggle('no-scroll');
+
+    // Calculate scaling
+    const buttonHeight = mobileNavToggle.offsetHeight;
+    const windowHeight = window.innerHeight;
+    const scaling = windowHeight / buttonHeight;
+
+    // Set scaling
+    if (el.classList.contains('is-visible')) {
+      el.style.transform = 'scale(' + scaling + ')';
+    } else {
+      el.style.transform = null;
+    }
   });
+
 
 }
 
